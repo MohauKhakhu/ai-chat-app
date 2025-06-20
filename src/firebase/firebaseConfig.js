ec327b5c69f4312a001c0d40638ca4e6bcf81a6f
MohauKhakhu <khakhumohau@gmail.com>
2025-06-20 09:58:25 +0200

    n

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "YOUR_AIzaSyAW665ouNYUw4d6gQNKzx2k-fAAWWPnUTI",
   authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "aichatapp-9da46",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
